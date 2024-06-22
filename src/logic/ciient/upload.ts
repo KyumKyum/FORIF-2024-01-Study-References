7f908a0e6a3bc810b1df6b4213f426e4d4a897ae
Jay Lim <mycheesepasta@gmail.com>
2024-06-22 16:24:39 +0900

    feat(url): applied

--- src/logic/ciient/upload.ts
+++ src/logic/ciient/upload.ts
@@ -12,16 +12,15 @@ const generateRandomString = (length: number): string => {
     return result;
 }
 
-const uploadFile = async (file: File, content: string) => {
-    const url = "http://localhost:3000/api/upload"
-    const name = generateRandomString(10);
-
+const uploadFile = async (file: File, name: string, content: string, youtubeUrl: string) => {
+    const url = "http://localhost:3000/api/image"
     const form = new FormData();
 
     // FormData에 필드 추가
     form.append('name', name);
     form.append('file', file);
     form.append('content', content);
+    form.append('youtube', youtubeUrl)
 
     await axios.post(url, form, {
         headers: {
