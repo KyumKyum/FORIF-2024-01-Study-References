248020e607833dbb9fe899db0b5b4c34136c97d6
Jay Lim <mycheesepasta@gmail.com>
2024-06-22 16:33:40 +0900

    build(script): redis

--- src/scripts/setup.sh
+++ src/scripts/setup.sh
@@ -8,6 +8,12 @@ if docker container inspect postgres >/dev/null 2>&1; then
   echo
 fi
 
+if docker container inspect redis >/dev/null 2>&1; then
+  echo 'Container for redis exists! Terminating the container...'
+  docker stop redis
+  echo
+fi
+
 ####### POSTGRESQL #######
 echo "[POSTGRESQL] Initiating set up for PostgreSQL..."
 
@@ -69,5 +75,43 @@ if [ "$DB_EXISTS" != "1" ]; then
 else
   echo "Database 'exampledb' already exists."
 fi
+
+######## REDIS #######
+echo "[REDIS] Initiating setups for redis..."
+
+# Check and stop running postgres container
+
+docker run \
+  -p 6379:6379 \
+  -v redis_volume:/data \
+  --rm --name redis\
+  -d redis:latest || { echo "❗️Redis Container failed to start"; exit 1; }
+
+echo "Redis Container started! Testing the connection..."
+
+MAX_RETRIES=10
+WAIT_SECONDS=2
+ATTEMPTS=0
+
+while true; do
+    response=$(docker exec -it redis redis-cli ping)
+    case $response in
+        *PONG*)
+            echo "✨ Connection Successful. Redis is up and running! ✨"
+            break
+            ;;
+        *)
+            ATTEMPTS=$((ATTEMPTS + 1))
+            echo "⚠️ Redis did not respond with PONG. Attempt $ATTEMPTS of $MAX_RETRIES. Retrying in $WAIT_SECONDS seconds..."
+
+            if [ $ATTEMPTS -eq $MAX_RETRIES ]; then
+                echo "❗️Failed to connect to Redis after $MAX_RETRIES attempts. Exiting."
+                exit 1
+            fi
+
+            sleep $WAIT_SECONDS
+            ;;
+    esac
+done
 
 echo "✨ Connection Successful! PostgreSQL is up and running! ✨"
